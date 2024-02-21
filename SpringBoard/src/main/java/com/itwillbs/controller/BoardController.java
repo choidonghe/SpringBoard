345443cbc85c605b8d951236a43e8f61ce979213
choidonghe <fsfsfs1@naver.com>
2024-02-21 16:39:16 +0900

    /board/register(POST) 작성완료

--- SpringBoard/src/main/java/com/itwillbs/controller/BoardController.java
+++ SpringBoard/src/main/java/com/itwillbs/controller/BoardController.java
@@ -1,19 +1,25 @@
 package com.itwillbs.controller;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.itwillbs.domain.BoardVO;
+import com.itwillbs.service.BoardService;
+
 @Controller
 @RequestMapping(value = "/board/*")
 public class BoardController {
 
-	
 	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
 	
-	
+	// 서비스 객체 주입
+	@Inject
+	private BoardService bService;
 	
 	//http://localhost:8088/board/register
 	
@@ -24,6 +30,24 @@ public class BoardController {
 		logger.debug(" /board/register.jsp 뷰 연결");
 	}
 	
+	// 글쓰기POST : /board/register
+	@RequestMapping(value = "/register",method = RequestMethod.POST)
+	public String registerPOST(BoardVO vo) throws Exception {
+		logger.debug(" /board/register.jsp (submit) -> registerPOST() 호출");
+		
+		// 한글처리(필터) 생략
+		// 전달정보 ( 글 정보 ) 저장
+		logger.debug("전달정보 : " + vo);
+		
+		// 서비스 -> DAO 글쓰기 동작 호출
+		bService.regist(vo);
+		
+		logger.debug(" 글쓰기 완료 ! -> 리스트 페이지로 이동 ");
+		
+		// 페이지 이동 (list)		
+		return "redirect:/board/list";
+				
+			}
 	
 	
 }
